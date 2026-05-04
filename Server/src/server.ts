bebded20be559fa044360a120d32cb4aa59a21d6
ameer ali vk <ameervk1875@gamil.com>
2026-05-04 11:42:38 +0530

    fix

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -13,22 +13,29 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-const url = process.env.FRONTED_URL;
-if (url) {
-  app.use(
-    cors({
-      origin: [url],
-      credentials: true,
-    }),
-  );
-}
+// const url = process.env.FRONTED_URL;
+// if (url) {
+//   app.use(
+//     cors({
+//       origin: [url],
+//       credentials: true,
+//     }),
+//   );
+// }
+app.use(cors({
+  origin: [
+    "https://blog-flow-three.vercel.app",
+    "https://blog-flow-fw5if6ytj-ameer-ali-vks-projects.vercel.app"
+  ],
+  credentials: true
+}));
 connectDB();
-connectRedis()
+connectRedis();
 
 app.use("/api/user/auth", userAuthRouter);
 app.use("/api/posts", postRouter);
 
-app.use(errorHandler)
+app.use(errorHandler);
 const port = process.env.PORT;
 
 app.listen(port, () => {
